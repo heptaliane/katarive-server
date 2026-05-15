74c5bfc174b8cee0903bff2a4c4804dc5628f4fa
heptaliane <heptaliane@gmail.com>
2026-05-16 01:41:21 +0900

    feat: Introduce generic Job and JobQueue interfaces
    This commit introduces generic `Job` and `JobQueue` interfaces, along with specific type aliases for `NarrationJob`, `SourceItemJob`, `SourceItemsJob`, and `SourceCollectionJob`. It also includes helper option types and functions for configuring these jobs. The accompanying mock implementations are also generated.
    
    This change lays the groundwork for a more structured and extensible job processing system within the service layer.

---
+++ internal/service/job/interface.go
@@ -0,0 +1,74 @@
+package job
+
+import (
+	"context"
+
+	pb "github.com/heptaliane/katarive-server/gen/pb/api/v1"
+
+	"github.com/heptaliane/katarive-server/internal/model"
+)
+
+//go:generate mockgen -source=$GOFILE -destination=mock/mock_$GOFILE -package=mock
+type Job[S, T any] interface {
+	Run(opts ...JobOption[S])
+	Result() *T
+	Status() pb.JobStatus
+}
+
+//go:generate mockgen -source=$GOFILE -destination=mock/mock_$GOFILE -package=mock
+type JobQueue[S, T any] interface {
+	Queue(ctx context.Context, opts ...JobOption[S])
+	Get(id string) *Job[S, T]
+}
+
+type NarrationJob Job[narrationJobOption, string]
+type SourceItemJob Job[sourceItemJobOption, model.SourceItem]
+type SourceItemsJob Job[sourceItemsJobOption, []*model.SourceSummary]
+type SourceCollectionJob Job[sourceCollectionJobOption, model.SourceCollection]
+type NarrationJobQueue JobQueue[narrationJobOption, string]
+type SourceItemJobQueue JobQueue[sourceItemJobOption, model.SourceItem]
+type SourceItemsJobQueue JobQueue[sourceItemsJobOption, []*model.SourceSummary]
+type SourceCollectionJobQueue JobQueue[sourceCollectionJobOption, model.SourceCollection]
+
+// helpers
+type JobOption[T any] = func(opt *T)
+
+type narrationJobOption struct {
+	url       string
+	narrator  string
+	speakerId int32
+}
+
+func WithNarrationUrl(url string) JobOption[narrationJobOption] {
+	return func(opt *narrationJobOption) { opt.url = url }
+}
+func WithNarrationNarrator(narrator string) JobOption[narrationJobOption] {
+	return func(opt *narrationJobOption) { opt.narrator = narrator }
+}
+func WithNarrationSpeakerId(speakerId int32) JobOption[narrationJobOption] {
+	return func(opt *narrationJobOption) { opt.speakerId = speakerId }
+}
+
+type sourceItemJobOption struct {
+	url string
+}
+
+func WithSourceItemUrl(url string) JobOption[sourceItemJobOption] {
+	return func(opt *sourceItemJobOption) { opt.url = url }
+}
+
+type sourceItemsJobOption struct {
+	url string
+}
+
+func WithSourceItemsUrl(url string) JobOption[sourceItemsJobOption] {
+	return func(opt *sourceItemsJobOption) { opt.url = url }
+}
+
+type sourceCollectionJobOption struct {
+	url string
+}
+
+func WithSourceCollectionUrl(url string) JobOption[sourceCollectionJobOption] {
+	return func(opt *sourceCollectionJobOption) { opt.url = url }
+}
